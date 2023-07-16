4885488e62ece982d0d48e3ff3b8032300828cd6
Dzmitry Tsitarau <mikstat90@gmail.com>
2023-07-16 17:10:06 +0200

    new commit

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -8,7 +8,7 @@ export const  Navbar = () => {
             <div className={s.item}><a href={'/dialogs'}>Messages</a></div>
             <div className={s.item}><a href={'/news'}>News</a></div>
             <div className={s.item}><a href={'/music'}>Music</a></div>
-            <div className={s.item}><a href={'/settings'}>Settings</a></div>
+            <div className={s.item}><a href={'/settings'}></a></div>
         </nav>
     );
 };
